fde32ec56ae923fe41783f3e2d9876700ae5872c
Amandinh0 <amanchhetricr7@gmail.com>
2022-10-20 22:48:33 -0400

    Code

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,97 @@
-package PACKAGE_NAME;public class Client {
+import java.util.Scanner;
+
+public class Client {
+    static notes user = new notes();
+
+    public static void main(String[] args) {
+        startProgram();
+        while(goAgain()){
+            startProgram();
+        }
+    }
+
+    public static void startProgram(){
+        String inputNote;
+        int inputTask;
+        System.out.println("The 12 widely used notes are" );
+        user.printNaturalNotes();
+        inputNote = ScanANote();
+        System.out.println("Chose one of the options");
+        System.out.println("Find a major scale : Enter 1");
+        System.out.println("Find a minor scale : Enter 2");
+        System.out.println("Find a major chord : Enter 3");
+        System.out.println("Find a minor chord : Enter 4");
+
+        inputTask = ScanATask();
+        switch(inputTask){
+            case 1:
+                user.getMajorNotes(inputNote);
+                break;
+
+            case 2:
+                user.getMinorNotes(inputNote);
+                break;
+
+            case 3:
+                user.getMajorChord(inputNote);
+                break;
+
+            case 4:
+                user.getMinorChord(inputNote);
+                break;
+
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
+
+    public static String ScanANote(){
+        System.out.println("Pick a note you want a scale or a chord for.");
+        System.out.println("Enter a note");
+        Scanner input = new Scanner(System.in);
+        String note = input.next();
+        while(!note.equals("A") && !note.equals("A#") &&!note.equals("B") &&!note.equals("C") &&!note.equals("C#") &&
+                !note.equals("D") &&!note.equals("Eb") &&!note.equals("E") &&!note.equals("F") &&!note.equals("F#") &&
+                !note.equals("G") &&!note.equals("G#")){
+            System.out.println("Please enter a valid note");
+            System.out.println("Pick a notes you want a scale or chord for.");
+            System.out.println("Enter a note");
+            note = input.next();
+        }
+        return note;
+    }
+
+    public static int ScanATask(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter you input");
+        int task = input.nextInt();
+        while(task != 1 && task != 2 && task != 3 && task != 4 ){
+            System.out.println("Invalid choice");
+            System.out.print("Please enter your choice again:");
+            task = input.nextInt();
+        }
+        return task;
+    }
+
+    public static boolean goAgain(){
+        System.out.println("Do you want to go again?(y/n)");
+        Scanner input = new Scanner(System.in);
+        String answer = input.next();
+        while((!(answer.equals("y"))) && (!(answer.equals("n")))){
+            System.out.println("Invalid Input");
+            System.out.println("Enter again.");
+            System.out.println("Do you want to go again?(y/n)");
+            answer = input.next();
+        }
+        switch(answer){
+            case "y":
+                return true;
+
+            case "n":
+                return false;
+
+        }
+        return false;
+    }
 }
--- src/notes.java
+++ src/notes.java
@@ -1,2 +1,118 @@
-package PACKAGE_NAME;public class notes {
+import java.util.Arrays;
+
+public class notes {
+
+    final private String Notes[] = {"A","A#", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#"};
+    private int note;
+    String[] currentScale = new String [8];
+    public notes(){
+        //empty
+    }
+
+    //get methods
+    public void getMajorNotes(String a){
+        setMajorNotes(a);
+        System.out.print("The " + a + " major notes are: ");
+        printTheScale(currentScale);
+    }
+
+    public void getMinorNotes(String a){
+        setMinorNotes(a);
+        System.out.print("The " + a + " minor notes are: ");
+        printTheScale(currentScale);
+    }
+    public void getMajorChord(String n){
+        setMajorNotes(n);
+        System.out.print("The " +n+ " major chord notes are: ");
+        System.out.println(currentScale[0] + ", "+ currentScale[2]+ ", "+ currentScale[4]);
+    }
+
+    public void getMinorChord(String n){
+        setMinorNotes(n);
+        System.out.print("The " +n+ " minor chord notes are: ");
+        System.out.println(currentScale[0] + ", "+ currentScale[2]+ ", "+ currentScale[4]);
+    }
+
+    //setter methods
+    public void setMajorNotes(String a){
+        note = findIndexof(a);
+        currentScale[0]= Notes[note];
+        currentScale[currentScale.length-1] = Notes[note];
+        for (int i = 1; i <= 6; i++) {
+            if (i != 3) {
+                currentScale[i] = wholestep(note);
+            } else {
+                currentScale[i] = halfStep(note);
+            }
+        }
+    }
+
+    public void setMinorNotes(String a){
+        note = findIndexof(a);
+        currentScale[0]= Notes[note];
+        currentScale[currentScale.length-1] = Notes[note];
+        for(int i = 1; i <= 6;i++){
+            if(i!=2 && i!=5){
+                currentScale[i] = wholestep(note);
+            }
+            else{
+                currentScale[i] = halfStep(note);
+            }
+        }
+    }
+    public int findIndexof(String a){
+        int start = 0;
+        for(int i = 0;i < Notes.length; i++){
+            if(a.equals(Notes[i])){
+                start = i;
+            }
+        }
+        return start;
+    }
+
+    public String wholestep(int n){
+        int starting_note = n;
+        if(starting_note == 10) {
+            note = 0;
+            return Notes[0];
+        }
+        else if(starting_note == 11) {
+            note = 1;
+            return Notes[1];
+        }
+        else {
+            note += 2;
+            return Notes[n + 2];
+        }
+
+    }
+
+    public  String halfStep(int n){
+        int starting_note = n;
+        if(starting_note == 11) {
+            note = 0;
+            return Notes[0];
+        }
+        else{
+            note += 1;
+            return Notes[starting_note + 1];
+        }
+
+    }
+
+    public void printTheScale(String [] a) {
+        System.out.print(a[0]);
+        for(int i = 1; i < a.length; i++){
+            System.out.print(", " + a[i]);
+        }
+        System.out.println();
+    }
+
+    public void printNaturalNotes(){
+        System.out.print(Notes[0]);
+        for(int i = 1; i < Notes.length; i++){
+            System.out.print(", "+Notes[i]);
+        }
+        System.out.println();
+    }
 }
